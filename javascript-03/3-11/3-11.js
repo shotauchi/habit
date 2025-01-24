4a388a9bfcb3cf6c0f3c965250fbd7771f00f358
[shotauchi] <[tianneixiang84@gmail.com]>
2025-01-24 20:20:34 +0000

    javascript-03~04

---
+++ javascript-03/3-11/3-11.js
@@ -0,0 +1,14 @@
+let mon1 = 'September';
+let mon2 = 'January';
+let mon3 = 'February';
+let mon4 = 'March';
+
+ if (mon1 == mon2) {
+    console.log('今月は' + mon2 +' です。');
+} else if (mon2 === mon3) {
+    console.log('今月は' + mon3 + 'です。');
+}  else if (mon3 != mon4) {
+      console.log('今月は' + mon1 + 'です。');
+  }else {
+    console.log('今月は' + mon4 + 'です。');
+}

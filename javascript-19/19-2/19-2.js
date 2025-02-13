0c6edd3481c915eb3012170549b39666f61fe072
[shotauchi] <[tianneixiang84@gmail.com]>
2025-02-13 19:31:39 +0000

    javascript-18~19

---
+++ javascript-19/19-2/19-2.js
@@ -0,0 +1,18 @@
+// Vueオブジェクトの新たなインスタンスの初期値
+let initApp = {
+  el:'#app',          // 関連付けるDOM要素
+  data : {            // データ
+    msg:'おはよう'
+  }
+};
+
+// Vueオブジェクトの新たなインスタンスを生成
+let app = new Vue(initApp);
+
+// Vueオブジェクトの新たなインスタンスを生成
+// let app = new Vue({
+//   el:'#app',          // 関連付けるDOM要素
+//   data : {            // データ
+//     msg:'おはよう'
+//   }
+// });
